8b486894781b74bf686688947a4e53c35a34c4c6
Vishnu Kumar <visnhu9027872285@gmail.com>
2026-08-06 01:06:52 +0530

    pre-docker part

--- src/mySaaS/views.py
+++ src/mySaaS/views.py
@@ -23,6 +23,27 @@ def home_page_view(request, *args, **kwargs):
     PageVisits.objects.create(path = request.path)
     return render(request, html_templates, my_context)
 
+def about_view(request, *args, **kwargs):
+    qs = PageVisits.objects.all()
+    page_qs = PageVisits.objects.filter(path = request.path)
+    try:
+        percent = (page_qs.count() * 100.0)/qs.count()
+    except:
+        percent = 0
+    my_title = "My title"
+    my_context = {
+        "page_title": my_title, 
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count()
+    }
+
+    path = request.path
+    print("path", path)
+    html_templates = "home.html" 
+    PageVisits.objects.create(path = request.path)
+    return render(request, html_templates, my_context)
+
 def my_old_home_page_view(request, *args, **kwargs):
     
     print(this_dir)
