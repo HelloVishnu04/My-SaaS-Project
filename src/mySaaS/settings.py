c8b79f2d76ac4569993a5ac212e87de22404bcef
krishna-gitcode <krishnakumarbooks@gmail.com>
2026-08-20 22:11:39 +0530

    DJANGO_SECRET_KEY GEN

--- src/mySaaS/settings.py
+++ src/mySaaS/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y*pr+f)(c29tm-(sxa5m(r1lspy8j&7n_2hh12zzn$dcjb@7%*"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+# DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
